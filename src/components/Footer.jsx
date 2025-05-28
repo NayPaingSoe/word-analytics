c6e24212ab6b95047e38f436871258482d409ffe
NayPaingSoe <naypaingsoe042@gmail.com>
2025-05-28 16:54:47 +0630

    fix: fetch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react"
 
 export default function Footer() {
+  const [todos,setTodos] = useState([]);
+ const fetchApi = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const response = await res.json();
+  setTodos(response);
+  console.log(response);
+ }
+  useEffect(() => {
+   fetchApi();
+  }, []);
   return (
     <div>
-      Footer
+      <button style={{padding: '10px'}}>Click</button>
+      {todos && (  <ul>
+        {todos.map(todo=>
+        (<li key={todo.title}>{todo.title}</li>)
+        )}
+      </ul>)}
+    
     </div>
   )
 }
